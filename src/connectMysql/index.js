229a3b77b9e1bbc79c9ac3b8bcee2ba57af85131
lupp <m17710499106_1@163.com>
2020-09-01 19:36:36 +0800

    add sweet

--- src/connectMysql/index.js
+++ src/connectMysql/index.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   port: '3306',
-  user: 'root',
-  password : '123456',
+  user: 'admin', // lupeng
+  password : '123456', // root
   database : 'fast_mail'
 });
  
